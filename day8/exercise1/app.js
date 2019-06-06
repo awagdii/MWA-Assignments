a4d209eb36499cfb9efef8b8793114603495fc09
Ahmed Wagdi <ahmed.wagdi.moslem@gmail.com>
2019-06-06 11:31:44 -0500

    Use .project

--- day8/exercise1/app.js
+++ day8/exercise1/app.js
@@ -13,10 +13,10 @@ client.connect(async (err) => {
     try {
 
         console.log(await collection.find({}).toArray());
-        console.log(await collection.find({}, { restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
-        console.log(await collection.find({}, { restaurant_id: 1, name: 1, district: 1, cuisine: 1, _id: 0 }).toArray());
-        console.log(await collection.find({}, { restaurant_id: 1, name: 1, district: 1, zipcode: 1, _id: 0 }).toArray());
-        console.log(await collection.find({district:'Bronx'}).toArray());
+        console.log(await collection.find({}).project({ restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
+        console.log(await collection.find({}).project({ restaurant_id: 1, name: 1, district: 1, cuisine: 1, _id: 0 }).toArray());
+        console.log(await collection.find({}).project({ restaurant_id: 1, name: 1, district: 1, zipcode: 1, _id: 0 }).toArray());
+        console.log(await collection.find({ district: 'Bronx' }).toArray());
         console.log(await collection.find({ district: 'Bronx' }).limit(5).toArray());
         console.log(await collection.find({ district: 'Bronx' }).skip(5).limit(5).toArray());
         console.log(await collection.find({ 'address.coord': { $lt: -95.754168 } }).forEach((element) => { console.log(element) }));
@@ -25,15 +25,15 @@ client.connect(async (err) => {
         console.log(await collection.find({ name: { $regex: '^.*ces$' } }, { restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
         console.log(await collection.find({ name: { $regex: '^.*Reg.*$' } }, { restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
         console.log(await collection.find({ district: 'Bronx', cuisine: { $in: ["American ", "Chinese"] } }).toArray());
-        console.log(await collection.find({ district: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
-        console.log(await collection.find({ district: { $nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
-        console.log(await collection.find({ district: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] }, "grades.score": { $lt: 10 } }, { restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
-        console.log(await collection.find({ 'address.coord.1': { $lt: 52, $gt: 42 }, district: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { restaurant_id: 1, name: 1, address: 1, cuisine: 1 }).forEach((element) => { console.log(element) }));
-        console.log(await collection.find({}, { name: 1, _id: 0 }).sort({ name: 1 }).toArray());
-        console.log(await collection.find({}, { name: 1, _id: 0 }).sort({ name: -1 }).toArray());
-        console.log(await collection.find({}, { name: 1, district: 1, _id: 0 }).sort({ name: 1, district: -1 }).toArray());
+        console.log(await collection.find({ district: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }).project({ restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
+        console.log(await collection.find({ district: { $nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }).project({ restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
+        console.log(await collection.find({ district: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] }, "grades.score": { $lt: 10 } }).project({ restaurant_id: 1, name: 1, district: 1, cuisine: 1 }).toArray());
+        console.log(await collection.find({ 'address.coord.1': { $lt: 52, $gt: 42 }, district: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }).project({ restaurant_id: 1, name: 1, address: 1, cuisine: 1 }).forEach((element) => { console.log(element) }));
+        console.log(await collection.find({}).project({ name: 1, _id: 0 }).sort({ name: 1 }).toArray());
+        console.log(await collection.find({}).project({ name: 1, _id: 0 }).sort({ name: -1 }).toArray());
+        console.log(await collection.find({}).project({ name: 1, district: 1, _id: 0 }).sort({ name: 1, district: -1 }).toArray());
         console.log(await collection.find({ 'address.coord': { $type: 'double' } }).forEach((element) => { console.log(element) }));
-        console.log(await collection.find({ name: { $regex: '^Mad.*' } }, {  name: 1, 'address.coord.0': 1,'address.coord.1': 1 }).toArray());
+        console.log(await collection.find({ name: { $regex: '^Mad.*' } }).project({ name: 1, 'address.coord': 1 }).forEach((element) => { console.log(element) }));
 
 
         client.close();
