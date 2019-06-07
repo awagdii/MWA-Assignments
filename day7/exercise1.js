7af20108b1ad25b0be7f2d5a60747e1e408c1c58
Ahmed Wagdi <ahmed.wagdi.moslem@gmail.com>
2019-06-07 04:21:26 -0500

    Add Day7 Assignment

---
+++ day7/exercise1.js
@@ -0,0 +1,11 @@
+const BooksSchema = {
+    ISBN: '',
+    authors: ['', ''],
+    keywords: ['', ''],
+    borrowed_by: [
+        { studnet_name: '', return_date: '' },
+        { studnet_name: '', return_date: '' }
+    ]
+
+
+}
