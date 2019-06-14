44e3b6cf44838e93faf7fca7accea77aad48688d
Ahmed Wagdi <ahmed.wagdi.moslem@gmail.com>
2019-06-13 23:27:53 -0500

    Add bundle.js configs

--- day11/available.ts
+++ day11/available.ts
@@ -15,4 +15,6 @@ console.log(new Course());
 
 //tsc -init to initilize the ts with tsconfig.json
 //enable decorators by setting "experimentalDecorators": true
-// install webpack for bundling our application
+// install webpack for bundling our application 
+// npm i webpack-cli --save-dev
+//Add following line to the   "build": "webpack ./available.js -o ./bundle.js --devtool source-map"
